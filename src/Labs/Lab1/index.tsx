69e47d1f33a2f9ea3ebb15058f03f769266b21a9
jahanavisinha <sinha.j@northeastern.edu>
2024-09-22 15:53:07 -0400

    first commit

---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Index() {
+    return (
+        <div>
+            <h2>Lab 1</h2>
+            <h3>Exercise 1.1</h3>
+            <h3>Exercise 1.2</h3>
+            <h3>Exercise 1.3</h3>
+        </div>
+    );
+
+}
---
+++ src/Labs/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Lab2 from "./Lab2";
+import Lab1 from "./Lab1";
+
+export default function Index() {
+    return(
+        <div>
+            <h1>Labs</h1>
+            <Lab1 />
+            <Lab2 />
+        </div>
+    );
+}
