029587f23b3edf6af4831f3ffa1ce11d90bb027e
jahanavisinha <sinha.j@northeastern.edu>
2024-09-22 19:26:22 -0400

    completed tasks until 2.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Labs from "./Labs";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+      <HashRouter>
+          <Routes>
+              <Route path="/Labs/*" element={<Labs />} />
+          </Routes>
       <div>
         <Labs />
       </div>
+      </HashRouter>
   );
 }
 
