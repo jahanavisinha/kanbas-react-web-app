0c4afd34656a44e7ccb89666ede66048714423b1
jahanavisinha <sinha.j@northeastern.edu>
2024-09-22 20:14:11 -0400

    Completed 3.2.4 Account Screen Navigation

---
+++ src/Kanbas/Account/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Signin from "./Signin";
+import { Routes, Route, Navigate } from "react-router";
+import Signup from "./Signup";
+import Profile from "./Profile";
+export default function Account() {
+    return (
+        <div id="wd-account-screen">
+            <h2>Account</h2>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+                <Route path="/Signin" element={<Signin />} />
+                <Route path="/Profile" element={<Profile />} />
+                <Route path="/Signup" element={<Signup />} />
+            </Routes>
+        </div>
+    );
+}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router";
+import Account from "./Account";
+import Profile from "./Account/Profile";
+import Signup from "./Account/Signup";
 export default function Kanbas() {
     return (
         <div id="wd-kanbas">
             <h1>Kanbas</h1>
+            <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Account />} />
+            </Routes>
         </div>
-    );}
+    );
+}
