43b735d2001a50af0637358a941ff5785ca2f4aa
jahanavisinha <sinha.j@northeastern.edu>
2024-10-07 21:02:51 -0400

    4.5 styling the home screen

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,3 +1,54 @@
+// import CoursesNavigation from "./Navigation";
+// import Modules from "./Modules";
+// import Home from "./Home";
+// import Assignments from "./Assignments";
+// import AssignmentEditor from "./Assignments/Editor";
+// import { Navigate, Route, Routes } from "react-router";
+// import {FaAlignJustify} from "react-icons/fa6";
+// export default function Courses() {
+//     return (
+//         <div id="wd-courses">
+//             <h2 className="text-danger">
+//                 <FaAlignJustify className="me-4 fs-4 mb-1"/>
+//                 Course 1234 </h2>
+//             <hr/>
+//             {/*<table>*/}
+//             {/*    <tr>*/}
+//             {/*        <td valign="top">*/}
+//             <div className="d-flex">
+//                 <div className="d-none d-md-block">
+//                     <CoursesNavigation/>
+//                 {/*</td>*/}
+//                 {/*<td valign="top">*/}
+//                 </div>
+//                 <div className="flex-fill">
+//                     <Routes>
+//                         <Route path="/" element={<Navigate to="Home"/>}/>
+//                         <Route path="Home" element={<Home/>}/>
+//                         <Route path="Modules" element={<h2>Modules</h2>}/>
+//                         <Route path="Assignments" element={<Assignments/>}/>
+//                         <Route path="Assignments/:aid" element={<AssignmentEditor/>}/>
+//                         <Route path="People" element={<h2>People</h2>}/>
+//                     </Routes>
+//                 </div></div>
+//         {/*        </td>*/}
+//         {/*    </tr>*/}
+//         {/*</table>*/}
+//     <Routes>
+//         <Route path="/" element={<Navigate to="Home"/>}/>
+//                 <Route path="Home" element={<h3>Home</h3>}/>
+//                 <Route path="Modules" element={<Modules/>}/>
+//                 <Route path="Assignments" element={<h3>Assignments</h3>}/>
+//                 <Route path="Assignments/:aid" element={<h3>Assignment Editor</h3>}/>
+//                 <Route path="People" element={<h3>People</h3>}/>
+//             </Routes>
+//         </div>
+//
+//     );
+// }
+//
+
+
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
@@ -5,6 +56,7 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { Navigate, Route, Routes } from "react-router";
 import {FaAlignJustify} from "react-icons/fa6";
+import PeopleTable from "./People/Table";
 export default function Courses() {
     return (
         <div id="wd-courses">
@@ -18,8 +70,8 @@ export default function Courses() {
             <div className="d-flex">
                 <div className="d-none d-md-block">
                     <CoursesNavigation/>
-                {/*</td>*/}
-                {/*<td valign="top">*/}
+                    {/*</td>*/}
+                    {/*<td valign="top">*/}
                 </div>
                 <div className="flex-fill">
                     <Routes>
@@ -28,20 +80,20 @@ export default function Courses() {
                         <Route path="Modules" element={<h2>Modules</h2>}/>
                         <Route path="Assignments" element={<Assignments/>}/>
                         <Route path="Assignments/:aid" element={<AssignmentEditor/>}/>
-                        <Route path="People" element={<h2>People</h2>}/>
+                        <Route path="People" element={<PeopleTable />} />
                     </Routes>
                 </div></div>
-        {/*        </td>*/}
-        {/*    </tr>*/}
-        {/*</table>*/}
-    <Routes>
-        <Route path="/" element={<Navigate to="Home"/>}/>
-                <Route path="Home" element={<h3>Home</h3>}/>
-                <Route path="Modules" element={<Modules/>}/>
-                <Route path="Assignments" element={<h3>Assignments</h3>}/>
-                <Route path="Assignments/:aid" element={<h3>Assignment Editor</h3>}/>
-                <Route path="People" element={<h3>People</h3>}/>
-            </Routes>
+            {/*        </td>*/}
+            {/*    </tr>*/}
+            {/*</table>*/}
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<Navigate to="Home"/>}/>*/}
+            {/*    <Route path="Home" element={<h3>Home</h3>}/>*/}
+            {/*    <Route path="Modules" element={<Modules/>}/>*/}
+            {/*    <Route path="Assignments" element={<h3>Assignments</h3>}/>*/}
+            {/*    <Route path="Assignments/:aid" element={<h3>Assignment Editor</h3>}/>*/}
+            {/*    <Route path="People" element={<h3>People</h3>}/>*/}
+            {/*</Routes>*/}
         </div>
 
     );
