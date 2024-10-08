dcbac4883c8aa58d0d2f4e9d35bc675f7e5c6f1b
jahanavisinha <sinha.j@northeastern.edu>
2024-10-07 22:00:42 -0400

    fixed the black tab on each navigation

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -25,28 +25,49 @@
 // }
 
 import { MdDoNotDisturbAlt } from "react-icons/md";
-import { FaCheckCircle } from "react-icons/fa";
-import { BiImport } from "react-icons/bi";
+import { FaCheckCircle, FaBullhorn, FaChartBar, FaBell } from "react-icons/fa";
+import { BiImport, BiHome } from "react-icons/bi";
 import { LiaFileImportSolid } from "react-icons/lia";
-{/* Find more icons */}
+import { AiOutlineEye } from "react-icons/ai";
+
 export default function CourseStatus() {
     return (
-        <div id="wd-course-status" style={{ width: "300px" }}>
+        <div id="wd-course-status" className="ms-4" style={{ width: "300px" }}> {/* Apply Bootstrap margin-start */}
             <h2>Course Status</h2>
             <div className="d-flex">
                 <div className="w-50 pe-1">
-                    <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
-                        <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </button>
+                    <button className="btn btn-lg btn-secondary w-100 text-nowrap">
+                        <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
+                    </button>
                 </div>
                 <div className="w-50">
                     <button className="btn btn-lg btn-success w-100">
-                        <FaCheckCircle className="me-2 fs-5" /> Publish </button>
+                        <FaCheckCircle className="me-2 fs-5" /> Publish
+                    </button>
                 </div>
             </div><br />
             <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-                <BiImport className="me-2 fs-5" /> Import Existing Content </button>
+                <BiImport className="me-2 fs-5" /> Import Existing Content
+            </button>
             <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-                <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </button>
-            {/* Complete the rest of the buttons */}
+                <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons
+            </button>
+
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <BiHome className="me-2 fs-5" /> Choose Home Page
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <AiOutlineEye className="me-2 fs-5" /> View Course Screen
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <FaBullhorn className="me-2 fs-5" /> New Announcement
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <FaChartBar className="me-2 fs-5" /> New Analytics
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <FaBell className="me-2 fs-5" /> View Course Notifications
+            </button>
         </div>
-    );}
+    );
+}
