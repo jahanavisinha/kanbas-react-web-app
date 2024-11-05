888475320b16f9b0ebc53f41d9fa6494b816601a
jahanavisinha <sinha.j@northeastern.edu>
2024-11-04 21:56:15 -0500

    not working

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Signin.css";
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { setCurrentUser } from "./reducer";
+import { useDispatch } from "react-redux";
+import * as db from "../Database";
 export default function Signin() {
+    const [credentials, setCredentials] = useState<any>({});
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const signin = () => {
+        const user = db.users.find(
+            (u: any) => u.username === credentials.username && u.password === credentials.password);
+        if (!user) return;
+        dispatch(setCurrentUser(user));
+        navigate("/Kanbas/Dashboard");
+    };
+
     return (
-        <div className="signin-container">
-            <h2>Signin</h2>
-            <div className="signin-form">
-                <input className="form-control" placeholder="username" />
-                <input className="form-control" placeholder="password" type="password" />
-                <Link to="/Kanbas/Dashboard" className="btn btn-primary btn-block" id="wd-signin-btn">
-                    Signin
-                </Link>
-                <Link to="/Kanbas/Account/Signup" className="signup-link" id="wd-signup-link">Signup</Link>
-            </div>
+        <div id="wd-signin-screen">
+            <h1>Sign in</h1>
+            <input defaultValue={credentials.username}
+                   onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+                   className="form-control mb-2" placeholder="username" id="wd-username" />
+            <input defaultValue={credentials.password}
+                   onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                   className="form-control mb-2" placeholder="password" type="password" id="wd-password" />
+            <button onClick={signin} id="wd-signin-btn" className="btn btn-primary w-100" > Sign in </button>
+            <Link id="wd-signup-link" to="/Kanbas/Account/Signup"> Sign up </Link>
         </div>
-    );
-}
+    );}
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,76 +1,99 @@
 import React, { useState } from "react";
 import { useParams } from "react-router";
-import * as db from "../../Database";
-import ModulesControls from "./ModulesControls";
-import ModuleControlButtons from "./ModuleControlButtons";
-import { BsGripVertical } from "react-icons/bs";
-import LessonControlButtons from "./LessonControlButtons";
-import { addModule, editModule, updateModule, deleteModule }
-    from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
+import { BsGripVertical } from "react-icons/bs";
+import ModulesControls from "./ModulesControls"; // Ensure the correct path
+import ModuleControlButtons from "./ModuleControlButtons";
+import LessonControlButtons from "./LessonControlButtons";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer"; // Ensure the correct path
 
 export default function Modules() {
     const { cid } = useParams();
-    const [modules, setModules] = useState<any[]>(db.modules);
     const [moduleName, setModuleName] = useState("");
 
+    // Access modules from Redux store
     const { modules } = useSelector((state: any) => state.modulesReducer);
     const dispatch = useDispatch();
 
-    const addModule = () => {
-        setModules([...modules, {
-            _id: new Date().getTime().toString(),
-            name: moduleName,
-            course: cid,
-            lessons: []
-        }]);
+    const handleAddModule = () => {
+        dispatch(addModule({ name: moduleName, course: cid }));
         setModuleName("");
     };
 
-    const deleteModule = (moduleId: string) => {
-        setModules(modules.filter((m) => m._id !== moduleId));
+    const handleDeleteModule = (moduleId: string) => {
+        dispatch(deleteModule(moduleId));
     };
 
-    const editModule = (moduleId: string) => {
-        setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
+    const handleEditModule = (moduleId: string) => {
+        dispatch(editModule(moduleId));
     };
 
-    const updateModule = (updatedModule: any) => {
-        setModules(modules.map((m) => (m._id === updatedModule._id ? updatedModule : m)));
+    const handleUpdateModule = (module: any) => {
+        dispatch(updateModule(module));
     };
 
     return (
-        <div className="wd-modules">
-            <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} />
+        <div className="container">
+            <div className="wd-modules">
+                <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
+                                 addModule={() => {
+                                     dispatch(addModule({ name: moduleName, course: cid }));
+                                     setModuleName("");
+                                 }} />
+
             <ul id="wd-modules" className="list-group rounded-0">
                 {modules
-                    .filter((module) => module.course === cid)
-                    .map((module) => (
-                        <li key={module._id} className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-                            <div className="wd-title p-3 ps-2 bg-secondary">
-                                <BsGripVertical className="me-2 fs-3" />
-                                {!module.editing ? (
-                                    module.name
-                                ) : (
-                                    <input
-                                        className="form-control w-50 d-inline-block"
-                                        onChange={(e) => updateModule({ ...module, name: e.target.value })}
-                                        onKeyDown={(e) => {
-                                            if (e.key === "Enter") {
-                                                updateModule({ ...module, editing: false });
+                    .filter((module: any) => module.course === cid)
+                    .map((module: any) => (
+                        <li
+                            key={module._id}
+                            className="wd-module list-group-item p-0 mb-5 fs-5 border-gray"
+                        >
+                            <div className="wd-title p-3 ps-2 bg-secondary d-flex align-items-center justify-content-between">
+                                <div className="d-flex align-items-center">
+                                    <BsGripVertical className="me-2 fs-3" />
+                                    {!module.editing ? (
+                                        <span>{module.name}</span>
+                                    ) : (
+                                        <input
+                                            className="form-control w-50 d-inline-block"
+                                            onChange={(e) =>
+                                                handleUpdateModule({
+                                                    ...module,
+                                                    name: e.target.value,
+                                                })
                                             }
-                                        }}
-                                        defaultValue={module.name}
-                                    />
-                                )}
-                                <ModuleControlButtons moduleId={module._id} deleteModule={deleteModule} editModule={editModule} />
+                                            onKeyDown={(e) => {
+                                                if (e.key === "Enter") {
+                                                    handleUpdateModule({
+                                                        ...module,
+                                                        editing: false,
+                                                    });
+                                                }
+                                            }}
+                                            defaultValue={module.name}
+                                        />
+                                    )}
+                                </div>
+                                <ModuleControlButtons
+                                    moduleId={module._id}
+                                    deleteModule={handleDeleteModule}
+                                    editModule={handleEditModule}
+                                />
                             </div>
-                            {module.lessons && (
+
+                            {/* Render lessons if they exist */}
+                            {module.lessons && module.lessons.length > 0 && (
                                 <ul className="wd-lessons list-group rounded-0">
-                                    {module.lessons.map((lesson) => (
-                                        <li key={lesson._id} className="wd-lesson list-group-item p-3 ps-1">
-                                            <BsGripVertical className="me-2 fs-3" />
-                                            {lesson.name}
+                                    {module.lessons.map((lesson: any) => (
+                                        <li
+                                            key={lesson._id}
+                                            className="wd-lesson list-group-item p-3 ps-1 d-flex align-items-center justify-content-between"
+                                        >
+                                            <div className="d-flex align-items-center">
+                                                <BsGripVertical className="me-2 fs-3" />
+                                                <span>{lesson.name}</span>
+                                            </div>
                                             <LessonControlButtons />
                                         </li>
                                     ))}
