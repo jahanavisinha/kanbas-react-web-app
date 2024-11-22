cdb7f922dc747b3746631188bd117c729ca2ce5c
jahanavisinha <sinha.j@northeastern.edu>
2024-11-22 16:52:23 -0500

    finished until 3.4.6

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -2,6 +2,7 @@ import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
     return (
@@ -16,6 +17,7 @@ export default function Lab5() {
             <PathParameters />
             <QueryParameters />
             <WorkingWithObjects />
+            <WorkingWithArrays />
 
         </div>
     );
