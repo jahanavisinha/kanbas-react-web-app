f314aae648a4d21791c50b2dbde6b0a0783189ff
jahanavisinha <sinha.j@northeastern.edu>
2024-12-08 16:50:59 -0500

    initial commit

--- src/Kanbas/reducer.ts
+++ src/Kanbas/reducer.ts
@@ -1,3 +1,32 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// const initialState = {
+//     enrollments: [],
+// };
+// const enrollmentSlice = createSlice({
+//     name: "enrollments",
+//     initialState,
+//     reducers: {
+//         setEnrollment: (state, {payload: newEnrollment}) => {
+//             state.enrollments = newEnrollment;
+//         },
+//         enroll: (state, { payload: { userId, courseId } }) => {
+//             const newEnrollment: any = {
+//                 _id: new Date().getTime().toString(),
+//                 user: userId,
+//                 course: courseId
+//             };
+//             state.enrollments = [...state.enrollments, newEnrollment] as any;
+//         },
+//         unenroll: (state, { payload: enrollmentId }) => {
+//             state.enrollments = state.enrollments.filter(
+//                 (m: any) => m._id !== enrollmentId);
+//         }
+//     },
+// });
+// export const { setEnrollment, enroll, unenroll } =
+//     enrollmentSlice.actions;
+// export default enrollmentSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     enrollments: [],
@@ -8,6 +37,12 @@ const enrollmentSlice = createSlice({
     reducers: {
         setEnrollment: (state, {payload: newEnrollment}) => {
             state.enrollments = newEnrollment;
+        },
+        addEnrollment: (state, {payload: newEnrollment}) => {
+            state.enrollments = [...state.enrollments, newEnrollment] as any;
+        },
+        deleteEnrollment: (state, {payload: deletedEnrollment}) => {
+            state.enrollments = state.enrollments.splice(deletedEnrollment);
         },
         enroll: (state, { payload: { userId, courseId } }) => {
             const newEnrollment: any = {
@@ -23,6 +58,6 @@ const enrollmentSlice = createSlice({
         }
     },
 });
-export const { setEnrollment, enroll, unenroll } =
+export const { setEnrollment, addEnrollment, deleteEnrollment, enroll, unenroll } =
     enrollmentSlice.actions;
 export default enrollmentSlice.reducer;
